2fef64f39a121b6fb356c27192323a7e9f644be6
jacoblamp <ja124052@ucf.edu>
2025-06-30 08:46:03 -0400

    Updated Ph Plots
    This commit consolidates 5 commits made to this file. I have deleted the other commits, including a merge, and replaced them with this commit.

--- Analysis/Ph_Plots.py
+++ Analysis/Ph_Plots.py
@@ -1,24 +1,22 @@
-# Plot pH as a function of failure time, for sensors U1 and U4
-
-import adds
+# plots Ph as a function of Time to Failure (ms), colored by solution type
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import adds
 
 def plot_ph():
-    # read file with the organized Ph data
-    ph_df=adds.get_master()
+    
+    df=adds.get_master()
 
-    # split data into 2 data frames based on sensor type
-    ph_df1=ph_df[ph_df["Sensor"]=="U1"]
-    ph_df4=ph_df[ph_df["Sensor"]=="U4"]
+    # remove solutions with no recorded Ph so they don't take up space in the legend
+    df=df[(df["Solution"]=="Adipic Acid - 1.24mM")|(df["Solution"]=="Adipic Acid - 0.712mM")|(df["Solution"]=="Adipic Acid - 0.388mM")|(df["Solution"]=="Succinic 0.388mM")]
 
-    # plot Time to Failure vs. Ph based on sensor type
-    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
-    sns.scatterplot(x="Time to Failure (ms)", y="Ph", data=ph_df1, ax=axes[0], color="blue")
-    axes[0].set_title('Time to Failure vs. Ph for U1')
-    sns.scatterplot(x="Time to Failure (ms)", y="Ph", data=ph_df4, ax=axes[1], color="red")
-    axes[1].set_title('Time to Failure vs. Ph for U4')
-    axes[0].tick_params(axis='x', rotation=45)
-    axes[1].tick_params(axis='x', rotation=45)
+    # plot
+    sns.scatterplot(x="Time to Failure (ms)", y="Ph", data=df, hue="Solution").set_title("Time to Failure (ms) vs. Ph by Solution Type")
+
+    # move legend to the right of the plot
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0., title="Solution")
+
     plt.show()
+    
 plot_ph()
